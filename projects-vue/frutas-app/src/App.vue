128096ca8aed4321a6d2bfa80084311089224dae
dragonnomada <dragonnomada123@gmail.com>
2021-12-02 10:58:00 -0600

    Session 4

---
+++ projects-vue/frutas-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <fruta-price-card></fruta-price-card> --> <!-- HTML -->
+  <!-- <FrutaPriceCard /> --> <!-- XML -->
+  <FrutaPriceCard />
+</template>
+
+<script>
+import FrutaPriceCard from './components/FrutaPriceCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FrutaPriceCard
+  }
+}
+</script>
---
+++ projects-vue/frutas-app/src/components/FrutaPriceCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="flex flex-col">
+            <div class="flex flex-row">
+                <div></div>
+                <div></div>
+            </div>
+            <div class="flex flex-row">
+                <div class="px-2" 
+                    v-for="tag in ['fruta', 'rojo', 'redondo', 'dulce']" 
+                    v-bind:key="tag">
+                    <div class="bg-green-500 hover:bg-green-700 
+                        text-white rounded px-2 py-1 
+                        cursor-pointer select-none">
+                        <span>{{ tag }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
