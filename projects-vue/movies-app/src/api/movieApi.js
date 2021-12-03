b7190eace9d56cdbfc067736d849935268d52d06
dragonnomada <dragonnomada123@gmail.com>
2021-12-03 11:42:24 -0600

    Session 5

---
+++ projects-vue/movies-app/src/api/movieApi.js
@@ -0,0 +1,59 @@
+let baseUrl = 'http://localhost:5000'
+
+export async function allMovies() {
+    const response = await fetch(`${baseUrl}/api/movies`)
+
+    if (response.status !== 200) {
+        const error = await response.text()
+        throw new Error(error)
+    }
+
+    const movies = await response.json()
+
+    return movies
+}
+
+export async function infoMovie(movieId) {
+    const response = await fetch(`${baseUrl}/api/movies/${movieId}`)
+
+    if (response.status !== 200) {
+        const error = await response.text()
+        throw new Error(error)
+    }
+
+    const movie = await response.json()
+
+    return movie
+}
+
+export async function directorByMovie(movieId) {
+    const response = await fetch(`${baseUrl}/api/movies/${movieId}/director`)
+
+    if (response.status !== 200) {
+        const error = await response.text()
+        throw new Error(error)
+    }
+
+    const director = await response.json()
+
+    return director
+}
+
+export async function addMovie(movie, directorId) {
+    const response = await fetch(`${baseUrl}/api/movies/add`, {
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ...movie, directorId })
+    })
+
+    if (response.status !== 200) {
+        const error = await response.text()
+        throw new Error(error)
+    }
+
+    const result = await response.json()
+
+    return result
+}
